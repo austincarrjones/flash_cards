068b5b36a3d3f340ea47b38878263fdf093a2e81
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-27 11:35:39 -0600

    refactor: formatting and eval notes

--- lib/card.rb
+++ lib/card.rb
@@ -11,4 +11,5 @@ class Card
 		@category = category
 	end
 
+	
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -11,12 +11,14 @@ end
 RSpec.describe Round do
 
   it 'exists' do
+    #this is called setup (before line break)
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     expect(round).to be_instance_of(Round)
     expect(round.deck).to eq(deck)
     expect(round.turns).to eq([])
@@ -31,6 +33,7 @@ RSpec.describe Round do
     deck = Deck.new(cards)
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+
     expect(new_turn.class).to eq(Turn)
     expect(new_turn.correct?).to eq true
     expect(round.turns).to eq([card_1])
@@ -50,6 +53,7 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     expect(round.number_correct).to eq(0)
     round.take_turn("Juneau")
     expect(round.number_correct).to eq(1)
@@ -64,9 +68,12 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     expect(number_correct_by_category(:Geography)).to eq(0)
+
     round.take_turn("Juneau")
     round.take_turn("Venus")
+
     expect(number_correct_by_category(:Geography)).to eq(1)
     expect(round.number_correct_by_category(:STEM)).to eq(0)
   end
@@ -78,9 +85,12 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     expect(round.percent_correct).to eq(0.0)
+
     round.take_turn("Juneau")
     round.take_turn("Venus")
+
     expect(round.percent_correct).to eq(50.0)
   end
 
@@ -91,9 +101,12 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     expect(round.percent_correct_by_category(:Geography)).to eq(0.0)
+
     round.take_turn("Juneau")
     round.take_turn("Venus")
+    
     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
     expect(round.percent_correct_by_category(:STEM)).to eq(0.0)
   end
